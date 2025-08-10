4658eda684191e28d6331c9a1a99f702fd832eba
Shanjida-Islam138 <shanjida.islam9938@gmail.com>
2025-08-10 08:58:56 +0600

    added pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar =() => {
+  return (
+    <div>
+      <div>
+        <img src={assets.logo} alt="" />
+      </div>
+    </div>
+  )
+}
+export default Navbar 
